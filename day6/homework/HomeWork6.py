e4e8330e4b878178b0a2bb7b98afaa43959cd39a
TsotneSekhno <csekhniashvili1@gmail.com>
2024-03-14 12:16:52 +0400

    second commit

---
+++ day6/homework/HomeWork6.py
@@ -0,0 +1,76 @@
+num1 =(10)
+num2 =(10.0)
+
+print(num1==num2)
+
+#>
+print(10>3)
+print(2>5)
+print(7>7)
+print(300>7000)
+print(1>7)
+
+#>=
+print(6>=0)
+print(4>=7)
+print(6>=1)
+print(5>=5)
+print(6>=90)
+
+#<
+print(1<0)
+print(4<0)
+print(8<8)
+print(7<8)
+print(4<6)
+
+#<=
+print(9>4)
+print(9>70)
+print(9>6)
+print(0>9)
+print(5>8)
+
+#==
+print(5==0)
+print(6==9)
+print(7==7)
+print(1==90)
+print(3==2)
+
+#!=
+print(13121!=5776)
+print(9!=6)
+print(51!=6)
+print(3!=3)
+print(3!=4)
+
+n3 =(9)
+n4 =(7)
+
+print(n3<n4)
+print(n3<=n4)
+print(n3>n4)
+print(n3>=n4)
+print(n3==n4)
+print(n3!=n4)
+
+
+#uyuyuuyu
+
+n5=(6)
+
+print(6>16)
+print(6>=16)
+print(6<16)
+print(6<=16)
+print(6==16)
+print(6!=16)
+
+#dfghtr0nfjnfgkjnjty-hnkhj[fhnketynfgjknfgnfhkm]
+
+number8 = input("enter number")
+number9 = input("enter second number")
+
+print(number8==number9)
+print(number8!=number9)
