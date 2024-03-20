3d9dd35601c2e9b1859f9e903ba3c58b7c640753
TsotneSekhno <csekhniashvili1@gmail.com>
2024-03-20 21:10:33 +0400

    second commit

---
+++ day7/goahomework/homeWorK7.py
@@ -0,0 +1,22 @@
+age = int(input("enter yout age: "))#16
+print(age<20 and age >12)
+
+page = float(input("Enter first number: "))#2.5
+page1 = int(input("Enter second number: "))#10
+print(page * page1)
+
+number = int(input("enter number: "))#108
+print(number>100 and number==108)
+
+num1 =int(input("Enter Number: "))#20
+num2 =int(input("Enter Number2: "))#50
+#and
+print(num1>300 and num2<1)
+print(num1<19 and num2>30)
+print(num1<21 and num2>500)
+print(num1>10 and num2<100)
+#or
+print(num1>39 or num2>51)
+print(num1>19 or num2>39)
+print(num1>1090 or num2<51)
+print(num1<19 or num2<90)
