e5718adcd0d4a3a923bad230de5947a6f05e435c
TsotneSekhno <csekhniashvili1@gmail.com>
2024-04-03 19:43:46 +0400

    second commit

---
+++ day 9/homework/homework9.py
@@ -0,0 +1,13 @@
+num1 = int(input("enter your salary: "))
+
+if num1>10000:
+    print("goashi swavlobdi")
+
+elif num1==(1000-10000):
+    print("you mid")
+
+elif num1<1000:
+    print("shemosuliyavi goashi matricelo")
+
+else:
+    print("goa")
