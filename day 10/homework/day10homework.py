21e2cdc3f757f7bb2701cca12c02775ff9947f36
TsotneSekhno <csekhniashvili1@gmail.com>
2024-04-14 20:35:14 +0400

    second commit

---
+++ day 10/homework/day10homework.py
@@ -0,0 +1,16 @@
+#1
+n1 = input("which program is it? ")#GOA
+
+if n1=="GOA":
+    print("its true")
+else:
+    print("cudi gadawyvetilebaa")
+
+#2
+n2 = int(input("price"))#100
+n3 = int(input("biujeti"))#50
+
+if n2>=n3:
+    print("yofnis")
+else:
+    print("ramdeni akldeba sayideli")
