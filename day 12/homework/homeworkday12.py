c5d9013099099631931f948e906ab4f6a997b0ba
TsotneSekhno <csekhniashvili1@gmail.com>
2024-04-26 20:49:52 +0400

    second commit

---
+++ day 12/homework/homeworkday12.py
@@ -0,0 +1,19 @@
+#n=-1
+#for i in range(5):
+   # n=n+2
+   # print(n)
+
+#n1=0
+#for i in range(11):
+    #n1=n1+5
+    #print(n1)
+
+#n2=int(input("enter your number :")) #20
+#for i in range(20):
+    #n2=n2-1
+   # print(n2)
+
+n4=-1
+for i in range(51):
+    n4=n4+1
+    print(n4)
