7d6dfc1d484f088c910921f4e36c567fe8f02551
TsotneSekhno <csekhniashvili1@gmail.com>
2024-07-31 13:08:10 +0400

    second commit

---
+++ day 34/homework/homework34.py
@@ -0,0 +1,22 @@
+#იმუშავეთ split-ის გამოყენებაზე: შექმენით 10 string და თითოეულზე გამოიყენეთ split ბრძანება,
+#  მოახდინეთ გახლეჩვა ნებისმიერი სიმბოლოთი4.
+
+
+#def find_short(s):
+    #list1 = s.split(" ")
+
+#    word_len = len(list1[0])
+ #   for i in list1:
+  #      if len(i) < word_len:
+   #         word_len = len(i)
+    
+    # word_len = 3
+    #return word_len
+
+#rint#(find_short("bitcoin take over the world maybe who knows perhaps"))
+
+a = "welcome to the jungle"
+
+x = a.split()
+
+print(x)
