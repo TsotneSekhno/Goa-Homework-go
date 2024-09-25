e2f3acd66e77f1510c7f9816f54eb15f51d57647
TsotneSekhno <csekhniashvili1@gmail.com>
2024-09-25 20:34:21 +0400

    second commit

---
+++ day 59/homework/script.js
@@ -0,0 +1,19 @@
+// document.getElementById("dudu")
+
+const p1=document.getElementById("dudu")
+
+p1.textContent="hello world"
+p1.style.color="yellow"
+p1.style.fontSize= 100
+p1.style.fontWeight=70
+p1.style.backgroundColor="black"
+
+
+const p1=document.getElementById("didu")
+
+
+p1.textContent="riodejaneiro"
+p1.style.color="black"
+p1.style.fontSize= 150
+p1.style.fontWeight=50
+p1.style.backgroundColor="green"
